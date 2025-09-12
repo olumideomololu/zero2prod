821f2ddf42a87b83a0377adb498fca9f1b0423e2
olumideomololu <oaomololu@gmail.com>
2025-09-12 17:11:18 +0100

    First Test Draft

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,20 @@
-use zero2prod::run;
-
 #[actix_web::test]
-async fn dummy_test() {
-    //
+async fn health_check_works() {
+    // Arrange
+    spawn_app().await.expect("Failed to spawn our app.");
+    let client = reqwest::Client::new();
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute reques.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app() -> std::io::Result<()> {
+    zero2prod::run().await
 }
