f2c781ebe49b477da7058577aba794fb90d5f16b
olumideomololu <oaomololu@gmail.com>
2025-09-19 21:48:20 +0100

    form extractor added

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,17 @@ use std::{io, net::TcpListener};
 
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
+
 async fn health_check() -> HttpResponse{
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -10,6 +20,7 @@ pub fn run(listner: TcpListener) -> Result<Server, io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listner)?
     .run();
